6db06eae5aa0ed4553c0757197faca2bb282f354
shitian <840872473@qq.com>
2018-08-19 15:34:49 +0800

    二分搜索树 学习代码

---
+++ src/main/java/com/wintig/tree/bst/BST.java
@@ -0,0 +1,82 @@
+package com.wintig.tree.bst;
+
+/**
+ * @author shitian
+ * @create 2018-08-18 下午9:43
+ */
+public class BST<E extends Comparable<E>> {
+
+
+    private Node root;
+
+    private int size;
+
+    public BST() {
+        root = null;
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void add(E e) {
+        root = add(root, e);
+    }
+
+    //返回插入新节点后二叉树的根
+    private Node add(Node node, E e) {
+
+        if (node == null) {
+            size ++;
+            return new Node(e);
+        }
+
+        if (e.compareTo(node.e) < 0) {
+            node.left = add(node.left, e);
+        } else if (e.compareTo(node.e) > 0) {
+            node.right = add(node.right, e);
+        }
+
+        return node;
+    }
+
+    public boolean contains(E e) {
+        return contains(root, e);
+    }
+
+    private boolean contains(Node node, E e) {
+
+        if (node == null) {
+            return false;
+        }
+
+        if (e.compareTo(node.e) == 0) {
+            return true;
+        } else if (e.compareTo(node.e) < 0) {
+            return contains(node.left, e);
+        } else {
+            return contains(node.right, e);
+        }
+
+    }
+
+    private class Node{
+
+        public E e;
+        private Node left;
+        private Node right;
+
+        public Node(E e) {
+            this.e = e;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+
+}
