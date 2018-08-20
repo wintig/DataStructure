8afd5a4beb0ed1af7ff926b7dab722591ec48c40
shitian <840872473@qq.com>
2018-08-20 20:32:25 +0800

    二分搜索树 删除任意节点 学习代码

--- src/main/java/com/wintig/tree/bst/BST.java
+++ src/main/java/com/wintig/tree/bst/BST.java
@@ -1,5 +1,7 @@
 package com.wintig.tree.bst;
 
+import com.sun.org.apache.bcel.internal.generic.IF_ACMPEQ;
+
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
@@ -228,6 +230,55 @@ public class BST<E extends Comparable<E>> {
         return node;
     }
 
+    //删除元素为e的节点
+    public void remove(E e) {
+
+        root = remove(root, e);
+    }
+
+    //删除以node为根的二分搜索树中值为e的节点，删除后返回二分搜索树的根
+    private Node remove(Node node, E e) {
+
+        if (node == null) {
+            return null;
+        }
+
+        if (e.compareTo(node.e) < 0) {
+            node.left = remove(node.left, e);
+            return node;
+        } else if (e.compareTo(node.e) > 0) {
+            node.right = remove(node.right, e);
+            return node;
+        } else { // e == node.e
+
+            //待删除左子树为null
+            if (node.left == null) {
+                Node rightNode = node.right;
+                node.right = null;
+                size --;
+                return rightNode;
+            }
+
+            //待删除右子数为null
+            if (node.right == null) {
+                Node leftNode = node.left;
+                node.left = null;
+                size --;
+                return leftNode;
+            }
+
+            //左右子数均不为null，找到比待删除节点大的最小节点，既待删除节点右子树的最小节点
+            //用这个节点顶替待删除节点的位置
+            Node successor = minimum(node.right);
+            successor.right = removeMin(node.right);    //这一步已经进行了size--;
+            successor.left = node.left;
+
+            node.left = node.right = null;
+            return successor;
+        }
+
+    }
+
     @Override
     public String toString(){
         StringBuilder res = new StringBuilder();
@@ -250,8 +301,9 @@ public class BST<E extends Comparable<E>> {
 
     private String generateDepthString(int depth){
         StringBuilder res = new StringBuilder();
-        for(int i = 0 ; i < depth ; i ++)
+        for(int i = 0 ; i < depth ; i ++){
             res.append("--");
+        }
         return res.toString();
     }
 
