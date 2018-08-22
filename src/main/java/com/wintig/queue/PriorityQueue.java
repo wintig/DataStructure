6ec3137e101028580786e657f54307e8652c21c7
shitian <840872473@qq.com>
2018-08-22 23:05:09 +0800

    优先队列

---
+++ src/main/java/com/wintig/queue/PriorityQueue.java
@@ -0,0 +1,43 @@
+package com.wintig.queue;
+
+import com.wintig.heap.MaxHeap;
+
+/**
+ * @ClassName PriorityQueue
+ * @Despriction 优先队列
+ * @Author shitian
+ * @Create 2018-08-22 下午8:59
+ */
+public class PriorityQueue<E extends Comparable<E>> implements Queue<E> {
+
+    private MaxHeap<E> maxHeap;
+
+    public PriorityQueue() {
+        maxHeap = new MaxHeap<>();
+    }
+
+    @Override
+    public int getSize() {
+        return maxHeap.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return maxHeap.isEmpty();
+    }
+
+    @Override
+    public void enqueue(E e) {
+        maxHeap.add(e);
+    }
+
+    @Override
+    public E dequeue() {
+        return maxHeap.extractMax();
+    }
+
+    @Override
+    public E getFront() {
+        return maxHeap.findMax();
+    }
+}
